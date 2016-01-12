ed291b36045a03723b8ae4e92478cfc930b4a85b
Radboud Visser <radboud@commucare.nl>
2016-01-12 15:10:53 +0100

    SharePrice class afgemaakt.

--- googleXChangeRate/SharePrice.cs
+++ googleXChangeRate/SharePrice.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace StockHelper
 {
@@ -15,6 +17,12 @@ namespace StockHelper
         public float getActualSharePrice(string token)
         {
             float result=0;
+            string format = "l1";
+            WebClient web = new WebClient();
+            string url = string.Format(@"http://finance.yahoo.com/d/quotes.csv?s={0}&f={1}", token, format);
+            byte[] sharePriceArray=web.DownloadData(url);
+            string sharePriceString = Encoding.ASCII.GetString(sharePriceArray).Replace("\n","");
+            result = XmlConvert.ToSingle(sharePriceString);
             return result;
         }
     }
